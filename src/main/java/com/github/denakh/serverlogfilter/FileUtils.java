15193e7d3d8c819cb64e432e727dde3d4b1d5932
Denakh <denakhokh@gmail.com>
2022-01-25 20:17:26 +0200

    Added base File utils

---
+++ src/main/java/com/github/denakh/serverlogfilter/FileUtils.java
@@ -0,0 +1,31 @@
+package com.github.denakh.serverlogfilter;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileUtils {
+
+    // "src/test/resources/file.log"
+    public static List<String> getStringLinesFromFile(String filePath) throws FileNotFoundException {
+        List<String> stringLinesList = new ArrayList<>();
+        Scanner scanner = new Scanner(new File(filePath));
+        while (scanner.hasNextLine()) {
+            stringLinesList.add(scanner.nextLine());
+        }
+        return stringLinesList;
+    }
+
+    public static File createFileFromString(String stringContent, String newFilePath) throws IOException {
+        File newFile = new File(newFilePath);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(newFile))) {
+            writer.write(stringContent);
+        }
+        return newFile;
+    }
+
+    public static LogItem getObjectFromStringLine(String stringLine) {
+        return new LogItem();
+    }
+}
